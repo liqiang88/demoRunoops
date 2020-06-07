bb6897cc7db22f3d2f2edc9f33ea7b8aea02ae92
liqiang <liqiang_88@yeah.net>
2020-06-07 13:10:45 +0800

    add demo file

--- database/mysql/runoops-demo-test.sql
+++ database/mysql/runoops-demo-test.sql
@@ -60,3 +60,78 @@ SELECT a.runoops_id, a.runoops_author, b.runoops_count FROM runoops_tbl a LEFT J
 
 -- LEFT JOIN右联
 SELECT a.runoops_id, a.runoops_author, b.runoops_count FROM runoops_tbl a RIGHT JOIN tcount_tbl b ON a.runoops_author = b.runoops_author;
+
+-- 使用 NULL 值
+CREATE TABLE runoops_test_tbl (runoops_author varchar(50) NOT NULL,runoops_count INT);
+INSERT INTO runoops_test_tbl (runoops_author, runoops_count) values ('runoops', 30);
+INSERT INTO runoops_test_tbl (runoops_author, runoops_count) values ('runoops自学教程', NULL);
+INSERT INTO runoops_test_tbl (runoops_author, runoops_count) values ('Google', NULL);
+INSERT INTO runoops_test_tbl (runoops_author, runoops_count) values ('FaceBook', 100);
+SELECT * FROM runoops_test_tbl;
+SELECT * FROM runoops_test_tbl WHERE runoops_count = NULL;
+SELECT * FROM runoops_test_tbl WHERE runoops_count != NULL;
+SELECT * FROM runoops_test_tbl WHERE runoops_count IS NULL;
+SELECT * FROM runoops_test_tbl WHERE runoops_count IS NOT NULL;
+
+-- MySQL 正则表达式
+SELECT * FROM runoops_tbl WHERE runoops_author REGEXP '^run'; -- 查找runoops_author字段中以'st'为开头的所有数据
+SELECT * FROM runoops_tbl WHERE runoops_author REGEXP 'com$'; -- 查找runoops_author字段中以'com'为结尾的所有数据
+SELECT * FROM runoops_tbl WHERE runoops_author REGEXP '自学教程'; -- 查找runoops_author字段中包含'自学教程'字符串的所有数据
+SELECT * FROM runoops_tbl WHERE runoops_author REGEXP '^[aeiou]|com$';-- 查找runoops_author字段中以元音字符开头或以'com'字符串结尾的所有数据
+
+-- MySQL 事务
+CREATE TABLE runoops_transaction_test( id int(5)) engine=innodb;  -- 创建数据表
+SELECT * FROM runoops_transaction_test;
+BEGIN;
+INSERT INTO runoops_transaction_test value(1);
+INSERT INTO runoops_transaction_test value(2);
+INSERT INTO runoops_transaction_test value(3);
+COMMIT;
+BEGIN;
+INSERT INTO runoops_transaction_test value(11);
+INSERT INTO runoops_transaction_test value(12);
+ROLLBACK;
+
+-- MySQL 临时表
+CREATE TEMPORARY TABLE sales_summary (
+product_name VARCHAR(50) NOT NULL
+,total_sales DECIMAL(12,2) NOT NULL DEFAULT 0.00
+,avg_unit_price DECIMAL(7,2) NOT NULL DEFAULT 0.00
+,total_units_sold INT UNSIGNED NOT NULL DEFAULT 0
+);
+
+INSERT INTO sales_summary
+(product_name, total_sales, avg_unit_price, total_units_sold)
+VALUES
+('cucumber', 100.25, 90, 2);
+
+SELECT * FROM sales_summary;
+
+DROP TABLE sales_summary;
+
+-- MySQL 复制表
+SHOW CREATE TABLE runoops_tbl \G;
+
+CREATE TABLE `clone_tbl` (
+  `runoops_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `runoops_title` varchar(100) NOT NULL,
+  `runoops_author` varchar(40) NOT NULL,
+  `added_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`runoops_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+INSERT INTO clone_tbl (runoops_id,runoops_title,runoops_author,added_date) SELECT runoops_id,runoops_title,runoops_author,added_date FROM runoops_tbl; -- 克隆表内容
+
+
+-- MySQL 序列使用
+CREATE TABLE insect(
+   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (id),
+    name VARCHAR(30) NOT NULL,
+    date DATE NOT NULL,
+    origin VARCHAR(30) NOT NULL
+);
+
+INSERT INTO insect (id,name,date,origin) VALUES(NULL,'housefly','2019-09-10','kitchen'),(NULL,'millipede','2019-09-10','driveway'),(NULL,'grasshopper','2019-09-10','front yard');
+
+SELECT * FROM insect ORDER BY id;
