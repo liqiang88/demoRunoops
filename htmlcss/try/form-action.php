70e976d3804fa0bb05b91a43bbe27da5b9368efd
luocz <2621996451@qq.com>
2020-08-04 10:23:12 +0800

    add try file

---
+++ htmlcss/try/form-action.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>自学教程(runoops.com) - form表单提交</title>
+</head>
+<body>
+<h2>接收到的输入如下：</h2>
+text=<?php echo isset($_GET['text']) ? $_GET['text'] : '';?>
+<p>该页面是从服务器为您返回的。服务器已经处理了您的输入，并返回结果。</p>
+
+<p>讲解服务器如何处理输入已经超出了 HTML 教程的范畴。如果您希望学习更多有关处理表单输入的知识，请阅读我们的 <a href="http://runoops.com/server/php/php-standard/php-tutorial.html">PHP教程</a>。</p>
+
+<p>请使用浏览器中的后退按钮返回刚才的例子。</p>
+</body>
+</html>
